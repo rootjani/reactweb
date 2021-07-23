f3935559823eda7704e8aac02fef34b8301a7ab1
rootjani <87577901+rootjani@users.noreply.github.com>
2021-07-23 10:17:58 +0500

    Add files via upload

---
+++ Robot.js
@@ -0,0 +1,53 @@
+export const robots = [
+
+{
+	id:1,
+	name:'Anas Rehman',
+	username: 'AR',
+	email:'anasrehman@gmail.com'
+
+},
+{
+	id:2,
+	name:'Hamza Shabbir',
+	username: 'HS',
+	email:'hamzashabbir@gmail.com'
+
+},
+{
+	id:3,
+	name:'OSAMA ZAMAN',
+	username: 'OZ',
+	email:'zamantransport@gmail.com'
+
+},
+{
+	id:4,
+	name:'Zaman Shoukat',
+	username: 'ZS',
+	email:'zamanshaukat@gmail.com'
+
+},
+{
+	id:6,
+	name:'OSAMA ZAMAN',
+	username: 'OZ',
+	email:'zamantransport@gmail.com'
+
+},
+{
+	id:7,
+	name:'Anas Rehman',
+	username: 'AR',
+	email:'anasrehman@gmail.com'
+
+},
+{
+	id:8,
+	name:'Anas Rehman',
+	username: 'AR',
+	email:'anasrehman@gmail.com'
+
+}
+
+];
